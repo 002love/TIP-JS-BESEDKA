b93260fc4707ba07f4e4718890c22182edf9dc44
Roman <107708360+Miku-Nakano-8888@users.noreply.github.com>
2022-11-16 22:31:56 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let ggg = (num) => {
-  nums = String(num).split('')
-  nums = nums.reduce((acum, el) => {return acum+(el**num.length)},0)
-  return num == nums ? true : false
-}
-
-console.log(ggg([7]))
-console.log(ggg([371]))
-console.log(ggg([1652]))
